e904ee588004bf1b57e1c88dd9458498cb1a21f3
Syed Shahir Ahmed Rakin <38401561+syedrakin707@users.noreply.github.com>
2025-11-01 01:07:50 +0600

    Delete BloodManagementSystem directory

--- BloodManagementSystem/blood/admin.py
+++
@@ -1,8 +0,0 @@
-# blood/admin.py
-from django.contrib import admin
-from .models import Profile, BloodBank, BloodDonationRequest, DonationOffer
-
-admin.site.register(Profile)
-admin.site.register(BloodBank)
-admin.site.register(BloodDonationRequest)
-admin.site.register(DonationOffer)
--- BloodManagementSystem/blood/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BloodConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blood'
